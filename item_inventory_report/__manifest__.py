6f10877a26ebf932b3ac15c786c61e83ddc27034
Muhammad Moosa <rckstar012@gmail.com>
2024-05-18 17:34:11 +0500

    [ADD] Item Inventory Report

---
+++ item_inventory_report/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Item Inventory Report',
+    'version' : '16.0.0.1',
+    'category': 'Inventory/Inventory',
+    'summary': 'Item Inventory Report',
+    'author': 'Muhammad Moosa',
+    'website': 'https://comstarusa.com',
+    'depends': ['base','product','stock', 'sale_management'],
+    'data': [
+        'security/ir.model.access.csv',
+        'wizard/inventory_report_wizard.xml',
+        'report/inventory_report.xml',
+
+        # 'views/product_sales_report_view.xml',
+        # 'report/product_sales_report_template.xml',
+    ],
+
+    'license': 'LGPL-3',}
---
+++ item_inventory_report/report/inventory_report.py
@@ -0,0 +1,78 @@
+from odoo import models, fields, api
+
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models, _
+import datetime
+import calendar
+from odoo.exceptions import UserError
+
+
+class ItemInventoryReportDocuments(models.AbstractModel):
+    _name = 'report.item_inventory_report.inventory_report_template'
+
+    @api.model
+    def _get_report_values(self, docids, data=None):
+        print("data", data)
+        start_date = False
+        end_date = False
+
+        if data['start_date']:
+            start_date = datetime.datetime.strptime(str(data['start_date']), '%Y-%m-%d').date()
+        if data['end_date']:
+            end_date = datetime.datetime.strptime(str(data['end_date']), '%Y-%m-%d').date()
+        data_dict = []
+
+        product_ids = self.env['product.product'].with_context(from_date=start_date, to_date=end_date).browse(
+            data['product_ids'])
+
+        # res = product_ids._compute_quantities_dict()
+
+        for product in product_ids:
+            default_code = product.default_code
+
+            # available_qty = self.env['stock.quant']._get_available_quantity(product, lot_id=False, strict=True)
+            if product.id == 25:
+                print("chalgaya")
+            location_ids = self.env['stock.location'].search([('usage', '=', 'internal')], limit=2)
+            move_lines = self.env['stock.move.line'].search([('date', '>=', data['start_date']), ('date', '<=', data['end_date']),
+                                                  ('product_id','=',product.id),('state','=','done')])
+            warehouse1_qty = sum(move_lines.filtered(lambda x:x.location_dest_id.id == location_ids[0].id and x.picking_type_id.code != 'incoming').mapped('qty_done'))
+            warehouse2_qty = sum(move_lines.filtered(lambda x:x.location_dest_id.id == location_ids[1].id and x.picking_type_id.code != 'incoming').mapped('qty_done'))
+
+        #     warehouse1_qty = self.env['stock.quant']._get_available_quantity(product, location_ids[0],
+        #                                                                      lot_id=False, strict=True)
+        #
+        # warehouse2_qty = self.env['stock.quant']._get_available_quantity(product, location_ids[1],
+        #                                                                  lot_id=False, strict=True)
+            available_qty = warehouse1_qty + warehouse2_qty
+
+            qty_sold_last_12_mo = sum(self.env['sale.report'].search(
+                [('date', '>=', data['start_date']), ('date', '<=', data['end_date']),
+                 ('product_id', '=', product.id)]).mapped('product_uom_qty'))
+
+            avg_use_per_month = round(qty_sold_last_12_mo / 12, 2)
+
+            if avg_use_per_month > 0 and available_qty > 0:
+                months_of_stock = round((available_qty / avg_use_per_month),
+                                        2)  # fixme available qty should be between start date and end date
+            else:
+                months_of_stock = available_qty if available_qty >= 0 else 0
+
+            data_dict.append({
+                'default_code': default_code,
+                'available_qty': available_qty,
+                'warehouse1_qty': warehouse1_qty,
+                'warehouse2_qty': warehouse2_qty,
+                'qty_sold_last_12_mo': qty_sold_last_12_mo,
+                'avg_use_per_month': avg_use_per_month,
+                'months_of_stock': months_of_stock,
+            })
+
+        return {
+            'doc_ids': docids,
+            'doc_model': 'item.inventory.report.wizard',
+            'docs': data_dict,
+            'start_date': start_date,
+            'end_date': end_date,
+        }
