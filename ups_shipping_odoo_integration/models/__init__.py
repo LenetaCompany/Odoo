befe6ba9baccc25c57bcf52b8646b191b78f3b3e
Yumna Sahar <ysahar@comstarusa.com>
2025-12-18 19:41:02 +0500

    Remove UPS module

--- ups_shipping_odoo_integration/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import delivery_carrier
-from . import package_details
-from . import res_company
-from . import sale_order
-from . import stock_picking
-from . import ups_thirdparty_account
-from . import ups_location
--- ups_shipping_odoo_integration/models/package_details.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields
-
-class PackageDetails(models.Model):
-    _inherit = 'stock.package.type'
-    package_carrier_type = fields.Selection(selection_add=[("ups_provider", "UPS")],
-                                            ondelete={'ups_provider': 'set default'})
