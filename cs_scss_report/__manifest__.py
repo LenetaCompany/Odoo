2380f49800a1e33e6325918459d01bbed25709f0
Fahad <fahadrizvi2@gmail.com>
2026-04-06 16:01:37 +0500

    Fix aged receivable report format

--- cs_scss_report/__manifest__.py
+++ cs_scss_report/__manifest__.py
@@ -1,39 +1,21 @@
 {
-    'name': 'Report css in odoo 19',
-     'version': '19.0.0.0',
-    'depends': ['account_reports'],
-
-    # 'assets': {
-    #     'web.report_assets_common': [
-    #         'cs_scss_report/static/src/css/account_pdf_override.css',
-    #
-    #     ],
-    #
-    # },
-
-
-'assets': {
-
-    'web.report_assets_common': [
-
-        'cs_scss_report/static/src/css/account_pdf_override.css',
-
-    ],
-
-    'web.report_assets_pdf': [
-
-        'cs_scss_report/static/src/css/account_pdf_override.css',
-
-    ],
-
-    'web.assets_backend': [
-
-        'cs_scss_report/static/src/css/account_pdf_override.css',
-
-    ],
-
-},
-
-'installable': True,
-    'application': False,
+    'name': 'Aged receivable format',
+    'version': '19.0.0.0',
+    'description': """Accounting Report """,
+    'category': 'contacts',
+    'summary': 'Aged receivable format',
+    'author': 'ComstarUSA',
+    'website': 'https://comstarusa.com',
+    'depends': ['account_accountant','account_reports'],
+    'assets': {
+        'account_reports.assets_pdf_export': [
+            'cs_scss_report/static/src/scss/cs_account_pdf_export_template.scss',
+        ],
+        'web.report_assets_common': [
+            'cs_scss_report/static/src/scss/cs_account_pdf_export_template.scss',
+        ],
+    },
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': True,
 }
